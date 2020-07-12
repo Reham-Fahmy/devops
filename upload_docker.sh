c6206e22eaf2a4d23e03e58edbb84d89d952b652
Reham-Fahmy <64305358+Reham-Fahmy@users.noreply.github.com>
2020-07-12 03:22:36 +0200

    Update upload_docker.sh

--- upload_docker.sh
+++ upload_docker.sh
@@ -4,14 +4,14 @@
 # Step 1:
 # Create dockerpath
 # dockerpath=<your docker ID/path>
-dockerpath="mohramedrafaat/sklearn-img"
+dockerpath="rehamfahmyhussien/sklearn-img"
 
 # Step 2:  
 # Authenticate & tag
 echo "Docker ID and Image: $dockerpath"
 sudo docker login
-docker tag sklearn-img mohamedrafaat/sklearn-img:sklearn-img
+docker tag sklearn-img rehamfahmyhussien/sklearn-img:sklearn-img
 
 # Step 3:
 #docker push
-docker push mohamedrafaat/sklearn-img:sklearn-img
+docker push rehamfahmyhussien/sklearn-img:sklearn-img
